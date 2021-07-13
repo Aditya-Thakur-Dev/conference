5784021db1639ca28cac90eb1ce0c1e84845feff
Aditya Thakur <atdthk@gmail.com>
2021-07-13 19:32:10 +0530

    home page

---
+++ src/main/java/com/condata/conference/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.condata.conference.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    @RequestMapping("/")
+    public String home(){
+        return "Welcome to the conferemce!!!";
+    }
+}
